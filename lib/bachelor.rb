8b3d096f923134c4a51d64933bc7f91ed8e026e0
Stella McEwen <mcewen.stella@gmail.com>
2019-04-18 21:18:59 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,4 +1,6 @@
 def get_first_name_of_season_winner(data, season)
+  winner = nil 
+  
   data.collect do |seasons, contestant_info|
   if seasons == season.to_sym
     contestant_info.collect do |stats|
