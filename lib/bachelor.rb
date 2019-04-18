8b97fddb60fba04a177230af5b433d0e47d51fb0
Stella McEwen <mcewen.stella@gmail.com>
2019-04-18 22:41:44 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -41,3 +41,8 @@ def get_occupation(data, hometown)
   end
 end
 
+def get_average_age_for_season(data, season)
+  data.each do |season_num, contestants|
+    contestants.each do |contestants_hash|
+      if
+end
