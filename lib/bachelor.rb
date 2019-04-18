007113696792640d28472d319d87e02be5e867c4
Stella McEwen <mcewen.stella@gmail.com>
2019-04-18 22:18:02 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,5 @@
 def get_first_name_of_season_winner(data,season)
- data[season.to_sym].each do |into_array|
+ data[season].each do |into_array|
    if into_array[:status] =="Winner"
      return into_array[:name].split(" ")[0]
    end
